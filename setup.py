2819a5ba90581b4d548889e3c748ed16578f3fe5
Diego Urrea <urreadiego767@gmail.com>
2021-12-10 20:13:57 -0500

    Libreria modular

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='score_rnx',
+    version='0.1',
+    description="Implementacion modular de metrica Rnx",
+    url="https://github.com/DiegoUrrea195/score_rnx",
+    author="Carlos David Correa, Diego Ferley Urrea",
+    license="MIT",
+    packages=['score_rnx'],
+    zip_safe=False
+)
